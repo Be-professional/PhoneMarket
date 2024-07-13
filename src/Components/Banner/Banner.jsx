5aa96e756457e58398dc61d030e438ce9a695fee
Shahin Alam <your-new-email@example.com>
2024-07-13 04:07:26 -0700

    bannae image add

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,9 +1,10 @@
 
 
 const Banner = () => {
+    const imgUrl = 'https://i.ibb.co/1X3yyD7/pexels-jessbaileydesign-788946.jpg'
     return (
         <div>
-            <h1 className="h-[70vh] bg-blue-gray-300">banner</h1>
+            <h1  className="h-auto bg-blue-gray-300"><img src={imgUrl} alt="" /></h1>
         </div>
     );
 };
